9bf6f401c891cac3ff0e138ebb068e5dbda3c081
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-06 14:45:28 +0200

    Component Video was finished.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {NavbarHome} from "../../components/NavbarHome/NavbarHome";
 import {Header} from "../../components/Header/Header";
+import {Video} from "../../components/Video/Video";
 
 export const Home = () => {
-  return (
-      <>
-        <NavbarHome/>
-        <Header/>
-      </>
-  );
+    return (
+        <>
+            <NavbarHome/>
+            <Header/>
+            <Video/>
+        </>
+    );
 };
