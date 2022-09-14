eb74d70487b01a16b0d589d36778eabec8174a96
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-14 17:12:31 +0200

    Component Info has been created and finished.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { Home } from './pages/Home/Home';
-import { List } from './pages/List/List';
-import { Car } from './pages/Car/Car';
+import {Home} from './pages/Home/Home';
+import {List} from './pages/List/List';
+import {Car} from './pages/Car/Car';
 import {Login} from "./pages/Login/Login";
 import {Register} from "./pages/Register/Register";
 import {Watch} from "./pages/Watch/Watch";
+import {Info} from "./pages/Info/Info";
 
 import './App.css';
 
 export const App = () => {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/cars" element={<List/>}/>
-          <Route path="/cars/:id" element={<Car/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/watch/:userId" element={<Watch/>}/>
-        </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/cars" element={<List/>}/>
+                <Route path="/cars/:id" element={<Car/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/watch/:userId" element={<Watch/>}/>
+                <Route path="/info" element={<Info/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
