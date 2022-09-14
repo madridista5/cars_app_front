ed429652347db1b09b170b82b1dff8589d68899b
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-14 16:30:41 +0200

    Function axiosData has been prepared.

---
+++ src/utils/axiosData.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import {apiUrl} from "../config/api";
+
+export const axiosData = axios.create({
+    baseURL: apiUrl,
+    headers: {
+        'Content-type': 'application/json',
+    },
+    withCredentials: true,
+});
