bd834da2e76f80bea66c3300919e9f280d102491
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-15 14:23:24 +0200

    UserContext has been prepared.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from './pages/Home/Home';
 import {List} from './pages/List/List';
@@ -7,21 +7,45 @@ import {Login} from "./pages/Login/Login";
 import {Register} from "./pages/Register/Register";
 import {Watch} from "./pages/Watch/Watch";
 import {Info} from "./pages/Info/Info";
+import {UserLoginResponse} from 'types';
+import {UserContext} from "./context/user.context";
 
 import './App.css';
 
 export const App = () => {
+    const [userData, setUserData] = useState<UserLoginResponse>({
+        id: '',
+        email: '',
+        role: '',
+        phoneNum: 0,
+        address: '',
+        lat: 0,
+        lon: 0,
+    });
+
+    useEffect(() => {
+        const data = localStorage.getItem('user');
+        if (data !== null) {
+            const userDataFromLS: UserLoginResponse = JSON.parse(data);
+            setUserData({
+                ...userDataFromLS,
+            });
+        }
+    }, []);
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/cars" element={<List/>}/>
-                <Route path="/cars/:id" element={<Car/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/watch/:userId" element={<Watch/>}/>
-                <Route path="/info" element={<Info/>}/>
-            </Routes>
-        </BrowserRouter>
+        <UserContext.Provider value={{...userData}}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/cars" element={<List/>}/>
+                    <Route path="/cars/:id" element={<Car/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/watch/:userId" element={<Watch/>}/>
+                    <Route path="/info" element={<Info/>}/>
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
     );
 };
