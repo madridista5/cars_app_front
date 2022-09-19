d9c4f9eac8c394b54dc9ca9657ef3f1396ac61ef
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-19 21:16:11 +0200

    Car view was created.

--- src/pages/Car/Car.tsx
+++ src/pages/Car/Car.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import {NavbarRoutes} from "../../components/NavbarRoutes/NavbarRoutes";
+import {Footer} from "../../components/Footer/Footer";
+
+import './Car.css';
 
 export const Car = () => {
     return (
-        <div>Car</div>
+        <>
+            <NavbarRoutes/>
+
+            <div className="app__bg car__wrapper section__padding">
+                car
+            </div>
+
+            <Footer/>
+        </>
     );
 };
