98aaefab026bd2732d78c553675f0d28b96f37e3
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-27 13:28:18 +0200

    Component AddImageForm was created.

---
+++ src/pages/AddImageForm/AddImageForm.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './AddImageForm.css';
+import {NavbarRoutes} from "../../components/NavbarRoutes/NavbarRoutes";
+import {Footer} from "../../components/Footer/Footer";
+
+export const AddImageForm = () => {
+
+    return (
+        <>
+            <NavbarRoutes/>
+            add image form
+            <Footer/>
+        </>
+    )
+}
